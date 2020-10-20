bcbed11f4d1eef8db2ebff989b15a1f4b352aa0d
CKKKKKKKKK <41709469+CKKKKKKKKK@users.noreply.github.com>
2020-10-20 14:31:56 +0800

    Commit Leetcode 116

---
+++ 116.Populating Next Right Pointers in Each Node/main.go
@@ -0,0 +1,28 @@
+package main
+
+type Node struct {
+	Val int
+	Left *Node
+	Right *Node
+	Next *Node
+}
+
+func connect(root *Node) *Node {
+	if root == nil || root.Left == nil {
+		return root;
+	}
+	leftPtr := root.Left;
+	rightPtr := root.Right;
+	for leftPtr != nil {
+		leftPtr.Next = rightPtr;
+		leftPtr = leftPtr.Right;
+		rightPtr = rightPtr.Left;
+	}
+	connect(root.Left);
+	connect(root.Right);
+	return root;
+}
+
+func main() {
+
+}
