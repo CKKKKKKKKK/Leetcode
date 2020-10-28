4d5801684df5fd6808032810d82d77c118e66ad2
CKKKKKKKKK <41709469+CKKKKKKKKK@users.noreply.github.com>
2020-10-28 08:23:41 +0800

    Commit Leetcode 1207

---
+++ 1207.Unique Number of Occurrences/Solution_1.go
@@ -0,0 +1,14 @@
+package uniqueOccurrences
+
+// Double hash table
+func uniqueOccurrences(arr []int) bool {
+    cnt := map[int]int {}
+    times := map[int]bool {}
+    for _, v := range arr {
+        cnt[v]++
+    }
+    for _, v := range cnt {
+        times[v] = true
+    }
+    return len(cnt) == len(times)
+}
