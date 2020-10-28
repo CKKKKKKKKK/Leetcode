58ed49e05755753c01b33274fe2ff7e42ddea3b8
CKKKKKKKKK <41709469+CKKKKKKKKK@users.noreply.github.com>
2020-10-28 08:30:02 +0800

    Update Solution_1.go

--- 1207.Unique Number of Occurrences/Solution_1.go
+++ 1207.Unique Number of Occurrences/Solution_1.go
@@ -2,13 +2,13 @@ package uniqueOccurrences
 
 // Double hash table
 func uniqueOccurrences(arr []int) bool {
-    cnt := map[int]int {}
-    times := map[int]bool {}
-    for _, v := range arr {
-        cnt[v]++
-    }
-    for _, v := range cnt {
-        times[v] = true
-    }
-    return len(cnt) == len(times)
+	cnt := map[int]int{}
+	times := map[int]bool{}
+	for _, v := range arr {
+		cnt[v]++
+	}
+	for _, v := range cnt {
+		times[v] = true
+	}
+	return len(cnt) == len(times)
 }
